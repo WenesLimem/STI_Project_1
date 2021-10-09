ba282f06cdd70c441e49868940137964e39400a0
Wènes Limem <32634307+WenesLimem@users.noreply.github.com>
2021-10-09 09:03:34 +0200

    Delete site directory

--- site/html/includes.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once $_SERVER['DOCUMENT_ROOT'].'/dataManagement.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'../control/Message.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '../control/User.php';
-session_start();
